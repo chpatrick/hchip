62d99a3532dc7908861bbd9a02d7401f12231192
Patrick Chilton <chpatrick@gmail.com>
2013-04-24 21:50:21 +0100

    Made DRW mostly pure IO.

--- HChip/Graphics.hs
+++ HChip/Graphics.hs
@@ -5,6 +5,8 @@ import Control.Applicative
 import Control.Lens
 import Control.Monad
 import Control.Monad.State
+import Data.Array.IO
+import Data.IORef 
 import Foreign
 import Foreign.Marshal.Array
 import Graphics.UI.SDL
@@ -19,15 +21,6 @@ cls = do
   liftIO $ fillRect bb Nothing (Pixel 0)
   return ()
 
-{-# INLINE setPixel #-}
-setPixel :: Ptr Word8 -> Int -> Int -> Int -> Word8 -> Emu ()
-setPixel scan0 pitch x y c
-  = when (x >= 0 && x < 320 && y >= 0 && y < 240 && c /= 0) $ do
-    let a = scan0 `plusPtr` (y * pitch + x)
-    cp <- liftIO $ peek a
-    when (cp /= (0 :: Word8)) (carry .= True) -- no idea why haskell can't infer this
-    liftIO $ poke a c
-
 flipCoord f m v
   = if f then m - v - 1 else v
 
@@ -40,14 +33,23 @@ drw rx ry a = do
   scan0 <- castPtr <$> liftIO (surfaceGetPixels s)
   let pitch = fromIntegral $ surfaceGetPitch s
   ( fh, fv ) <- use spriteFlip
-  carry .= False
-  forM_ [ (x, y) | y <- [0 .. h - 1], x <- [0 .. w - 1]] $ \( x, y ) -> do
-    pp <- load8 (Mem (a + fromIntegral y * fromIntegral w + fromIntegral x))
-    let sx1 = flipCoord fh (fromIntegral w * 2) (fromIntegral x * 2) + fromIntegral x0
-    let sx2 = flipCoord fh (fromIntegral w * 2) (fromIntegral x * 2 + 1) + fromIntegral x0
-    let sy = fromIntegral (flipCoord fv h y) + fromIntegral y0
-    setPixel scan0 pitch sx1 sy (highNibble pp)
-    setPixel scan0 pitch sx2 sy (lowNibble pp)
+  mem <- gets memory
+  c <- liftIO $ do
+    cr <- newIORef False
+    forM_ [ (x, y) | y <- [0 .. h - 1], x <- [0 .. w - 1]] $ \( x, y ) -> do
+      pp <- readArray mem (a + fromIntegral y * fromIntegral w + fromIntegral x)
+      let sx1 = flipCoord fh (fromIntegral w * 2) (fromIntegral x * 2) + fromIntegral x0
+      let sx2 = flipCoord fh (fromIntegral w * 2) (fromIntegral x * 2 + 1) + fromIntegral x0
+      let sy = fromIntegral (flipCoord fv h y) + fromIntegral y0
+      let setPixel sx c = when (sx >= 0 && sx < 320 && sy >= 0 && sy < 240 && c /= 0) $ do
+			      let a = scan0 `plusPtr` (sy * pitch + sx)
+			      cp <- peek a
+			      when (cp /= (0 :: Word8)) (writeIORef cr True) -- no idea why haskell can't infer this
+			      poke a c
+      setPixel sx1 (highNibble pp)
+      setPixel sx2 (lowNibble pp)
+    readIORef cr
+  carry .= c
 
 updatePalette :: Emu ()
 updatePalette = do
